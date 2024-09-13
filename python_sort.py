037d43e2831c3a69e095b3245b22ff3e43290c69
altugkarakurt <altugkarakurt@gmail.com>
2024-09-13 17:11:10 -0600

    Added bubblesort

--- python_sort.py
+++ python_sort.py
@@ -11,7 +11,6 @@ def insertion_sort(lst):
                 break
     return lst
 
-
 """--------------------------------------------------------------------
 We find the minimum in lst[i:] and use the head for the sorted sublist 
 --------------------------------------------------------------------"""
@@ -23,5 +22,44 @@ def selection_sort(lst):
                 min_item, min_idx = lj, (i+j)
         if(min_idx != i):
             li = lst[i]
-            lst = lst[:i] + [min_item] + [li] + lst[i+1:min_idx] + lst[min_idx+1:]
+            lst = lst[:i] + [min_item, li] + lst[i+1:min_idx] + lst[min_idx+1:]
     return lst
+
+"""--------------------------------------------------------------------
+Iterate over pairs of terms and order them and keep doing so until a
+pass completes without any reordering
+--------------------------------------------------------------------"""
+def bubble_sort(lst):
+    is_sorted = False
+    while(not is_sorted):
+        is_sorted = True
+        for i,li in enumerate(lst[:-1]):
+            if(li > lst[i+1]):
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+                is_sorted = False
+    return lst
+
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
+def shell_sort(lst):
+    pass
+
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
+def merge_sort(lst):
+    pass
+
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
+def heap_sort(lst):
+    pass
+
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
+def quick_sort(lst):
+    pass
+
+"""--------------------------------------------------------------------
+--------------------------------------------------------------------"""
+def quick3_sort(lst):
+    pass
