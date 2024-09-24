d867895922a90e7899c4f31f4ecc12531a1b23c8
altugkarakurt <altugkarakurt@gmail.com>
2024-09-23 22:52:13 -0600

    Stricter static typing in C/Cython search algorithms

--- c_search_sort/_c_search.c
+++ c_search_sort/_c_search.c
@@ -8,7 +8,7 @@
 */
 
 int linear_search(uint8_t item, uint8_t *lst, size_t lst_len) {
-    for(int i=0; i < lst_len; i++){
+    for(unsigned int i=0; i < lst_len; i++){
         if(lst[i] ==  item){
             return i;
         }
--- cython_search_sort/cy_search.pyx
+++ cython_search_sort/cy_search.pyx
@@ -7,7 +7,6 @@ import cython
 # cython.uchar = unsigned char = uint8_t in C.
 def cy_linear_search(item: cython.uchar, lst: cython.uchar[:]) -> cython.int:
     i: cython.int
-    li: cython.int
     for i in range(lst.shape[0]):
         if(item == lst[i]):
             return i
@@ -26,32 +25,3 @@ def cy_binary_search(item: cython.uchar, lst: cython.uchar[:]) -> cython.int:
         else:
             return mid_idx
     return -1
-
-"""
-def binary_search(item: cython.object, lst: cython.object) -> cython.int:
-    cy_item: cython.int = item
-    cy_list: cython.int[:] = array.array('i', lst)
-    left_idx: cython.int = 0
-    right_idx: cython.int = len(cy_list)-1
-    mid_idx: cython.int
-
-    while(left_idx <= right_idx):
-        mid_idx = int((left_idx + right_idx)/2)
-        if(cy_list[mid_idx] < cy_item):
-            left_idx = mid_idx + 1
-        elif(cy_list[mid_idx] > cy_item):
-            right_idx = mid_idx - 1
-        else:
-            return mid_idx
-    return None
-
-cdef int linear_search(item: cython.object, lst: cython.object):
-    cy_item: cython.int = item
-    cy_list: cython.int[:] = array.array('i', lst)
-    i: cython.int
-    li: cython.int
-    for i,li in enumerate(cy_list):
-        if(cy_item == li):
-            return i
-    return -1
-"""
